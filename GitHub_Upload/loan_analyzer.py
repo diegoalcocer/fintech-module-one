6c8065381ee43f127e757c678ed2a9c099ccccfa
Diego <alcocerd86@gmail.com>
2022-04-19 13:33:53 -0400

    Add solution for Part 3: Perform Financial Calculations

--- GitHub_Upload/loan_analyzer.py
+++ GitHub_Upload/loan_analyzer.py
@@ -123,13 +123,25 @@ new_loan = {
 # @TODO: Define a new function that will be used to calculate present value.
 #    This function should include parameters for `future_value`, `remaining_months`, and the `annual_discount_rate`
 #    The function should return the `present_value` for the loan.
-# YOUR CODE HERE!
 
+print("")
+print("Part 3: Perform Financial Calculations")
+print("------------------------------------------------------------------")
+
+
+def calculate_present_value(future_value, remaining_months, annual_discount_rate):
+    present_value = future_value / (1 + annual_discount_rate /12)**remaining_months
+    return present_value
 
 # @TODO: Use the function to calculate the present value of the new loan given below.
 #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
 # YOUR CODE HERE!
-print(f"The present value of the loan is: {present_value}")
+remaining_months = new_loan["remaining_months"]
+future_value = new_loan["future_value"]
+print(f"New Loan remaining months: {remaining_months}, future value: {future_value}, annual discount rate: {0.2}")
+present_value = calculate_present_value(future_value, remaining_months, 0.2)
+print(f"The present value of the New Loan is: {present_value:.2f}")
+print("------------------------------------------------------------------")
 
 
 """Part 4: Conditionally filter lists of loans.
